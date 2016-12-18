bb650e39ac4b2d99f1489e174d887280f3377e00
Lior <bialik@halo-analytics.com>
2016-12-18 17:42:10 +0200

    added some constants from the assignment

--- src/config.py
+++ src/config.py
@@ -1,6 +1,17 @@
 import argparse
 import os
 
+
+L1BlockSize = [2, 4, 8, 16, 32, 64, 128]
+L2BlockSize = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+# magics
+mainMemorySize = 16392
+L1MemorySize = 4098
+L2MemorySize = 32784
+L1HitTimeCycles = 1
+L2HitTimeCycles = 4
+
 options = None
 
 
@@ -10,26 +21,38 @@ def assertOptionsInit():
 
 def getCmdLineOptions():
     parser = argparse.ArgumentParser(description='Cache Simulation')
-    parser.add_argument('levels', type=assertValidLevels,
+
+    parser.add_argument('levels',
+                        type=assertValidLevels,
                         help='The levels of cache to use:\n 1 level is for L1 only.\n 2 levels is for both L1 and L2')
     parser.add_argument('b1',
                         help='The cache block size in decimal for L1')
     parser.add_argument('b2',
                         help='The cache block size in decimal for L2. only relevant if levels is 2')
-    parser.add_argument('traceFilePath', type=assertFileExists, default="trace.txt",
+    parser.add_argument('traceFilePath',
+                        type=assertFileExists,
+                        default="trace.txt",
                         help='The path to the trace file')
-    parser.add_argument('meminFilePath', type=assertFileExists, default="memin.txt",
+    parser.add_argument('meminFilePath',
+                        type=assertFileExists,
+                        default="memin.txt",
                         help='The path to the memin file which contains the main memory\'s status before a run')
-    parser.add_argument('memoutFilePath', default="memout.txt",
+    parser.add_argument('memoutFilePath',
+                        default="memout.txt",
                         help='The path to the memout file which contains the main memory\'s status after a run')
-    parser.add_argument('L1_memoutFilePath', default="l1.txt",
+    parser.add_argument('L1_memoutFilePath',
+                        default="l1.txt",
                         help='The path to the file which contains the L1 cache memory\'s status after a run')
-    parser.add_argument('L2_Way0_memoutFilePath', default="l2way0.txt",
+    parser.add_argument('L2_Way0_memoutFilePath',
+                        default="l2way0.txt",
                         help='The path to the file which contains the L2, way 0 cache memory\'s status after a run')
-    parser.add_argument('L2_Way1_memoutFilePath', default="l2way1.txt",
+    parser.add_argument('L2_Way1_memoutFilePath',
+                        default="l2way1.txt",
                         help='The path to the file which contains the L2, way 1 cache memory\'s status after a run')
-    parser.add_argument('statsFilePath', default="stats.txt",
+    parser.add_argument('statsFilePath',
+                        default="stats.txt",
                         help='The path to the stats file which contains the running stats after a run')
+
     cmdLineOptions = parser.parse_args()
 
     return cmdLineOptions
--- src/sim.py
+++ src/sim.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
         print(e.message)
         sys.exit(1)
 
-    print(config.options)
+
+

2a096164a34969897df5aae86c84df847c53faa0
Lior <bialik@halo-analytics.com>
2017-01-11 13:57:33 +0200

    added a memin generator and refactored tracefile generator

---
+++ Scripts/meminGenerator.py
@@ -0,0 +1,14 @@
+import random
+
+NUMBER_OF_MEMORY_DATA = 1000
+
+def generateHexNumber(length):
+    return ''.join([random.choice('0123456789ABCDEF') for x in range(length)])
+
+with open("memin.txt", 'w') as meminFile:
+    for i in range(NUMBER_OF_MEMORY_DATA):
+        if i > 0:
+            meminFile.write("\n")
+        meminFile.write(generateHexNumber(2))
+
+meminFile.close()
--- Scripts/traceFileGeenerator.py
+++ Scripts/traceFileGeenerator.py
@@ -1,7 +1,9 @@
 import random
 
+NUMBER_OF_COMMANDS = 100
+
 def generateHexNumber(length):
-   return ''.join([random.choice('0123456789ABCDEF') for x in range(length)])
+    return ''.join([random.choice('0123456789ABCDEF') for x in range(length)])
 
 def generateCommandsNumber():
     return random.randint(1, 100)
@@ -9,11 +11,12 @@ def generateCommandsNumber():
 command = ['L', 'S']
 
 with open("trace.txt", 'w') as traceFile:
-    for i in range(100):
+    for i in range(NUMBER_OF_COMMANDS):
         loadStoreVal = random.choice(command)
         traceLine = "{commandsNumber} {loadStore} {memoryAddress}{dataToWrite}\n".format(commandsNumber=generateCommandsNumber(),
-                                                                                           loadStore=loadStoreVal,
-                                                                                           memoryAddress=generateHexNumber(6),
-                                                                                           dataToWrite=' ' + generateHexNumber(8) if loadStoreVal is 'S' else '')
+                                                                                         loadStore=loadStoreVal,
+                                                                                         memoryAddress=generateHexNumber(6),
+                                                                                         dataToWrite=' ' + generateHexNumber(8) if loadStoreVal is 'S' else '')
         traceFile.write(traceLine)
 
+traceFile.close()

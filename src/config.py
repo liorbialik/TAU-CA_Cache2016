308cd4a91ed2120bfcdc90fb253449ddd59065de
Lior <bialik@halo-analytics.com>
2017-01-11 14:59:13 +0200

    added assertions for valid block size for l1,l2 cache

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ import argparse
 import os
 
 
-L1BlockSizes = [2, 4, 8, 16, 32, 64, 128]
-L2BlockSizes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+validL1BlockSizes = [2, 4, 8, 16, 32, 64, 128]
+validL2BlockSizes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
 # Memory Magics
 mainMemorySize = 16777216  # Bytes
@@ -15,7 +15,7 @@ MemoryAccessTime = 100
 secondaryMemoryAccessTime = 1
 
 CPUL1DataBusSize = 32  # bits
-CPUL1AddressBusSize= 24  # bits
+CPUL1AddressBusSize = 24  # bits
 L1L2BusSize = 256  # bits
 L1MemBusSize = 64  # bits - if not running with L2
 L2MemBusSize = 64  # bits - if running with L2
@@ -34,10 +34,10 @@ def getCmdLineOptions():
                         type=assertValidLevels,
                         help='The levels of cache to use:\n 1 level is for L1 only.\n 2 levels is for both L1 and L2')
     parser.add_argument('b1',
-                        type=assertValidBlockSize,
+                        type=assertValidL1BlockSize,
                         help='The cache block size in decimal for L1')
-    parser.add_arguent('b2',
-                        type=assertValidBlockSize,
+    parser.add_argument('b2',
+                        type=assertValidL2BlockSize,
                         help='The cache block size in decimal for L2. only relevant if levels is 2')
     parser.add_argument('traceFilePath',
                         type=assertFileExists,
@@ -47,21 +47,21 @@ def getCmdLineOptions():
                         type=assertFileExists,
                         default="memin.txt",
                         help='The path to the memin file which contains the main memory\'s status before a run')
-    # parser.add_argument('memoutFilePath',
-    #                     default="memout.txt",
-    #                     help='The path to the memout file which contains the main memory\'s status after a run')
-    # parser.add_argument('L1_memoutFilePath',
-    #                     default="l1.txt",
-    #                     help='The path to the file which contains the L1 cache memory\'s status after a run')
-    # parser.add_argument('L2_Way0_memoutFilePath',
-    #                     default="l2way0.txt",
-    #                     help='The path to the file which contains the L2, way 0 cache memory\'s status after a run')
-    # parser.add_argument('L2_Way1_memoutFilePath',
-    #                     default="l2way1.txt",
-    #                     help='The path to the file which contains the L2, way 1 cache memory\'s status after a run')
-    # parser.add_argument('statsFilePath',
-    #                     default="stats.txt",
-    #                     help='The path to the stats file which contains the running stats after a run')
+    parser.add_argument('memoutFilePath',
+                        default="memout.txt",
+                        help='The path to the memout file which contains the main memory\'s status after a run')
+    parser.add_argument('L1_memoutFilePath',
+                        default="l1.txt",
+                        help='The path to the file which contains the L1 cache memory\'s status after a run')
+    parser.add_argument('L2_Way0_memoutFilePath',
+                        default="l2way0.txt",
+                        help='The path to the file which contains the L2, way 0 cache memory\'s status after a run')
+    parser.add_argument('L2_Way1_memoutFilePath',
+                        default="l2way1.txt",
+                        help='The path to the file which contains the L2, way 1 cache memory\'s status after a run')
+    parser.add_argument('statsFilePath',
+                        default="stats.txt",
+                        help='The path to the stats file which contains the running stats after a run')
 
     cmdLineOptions = parser.parse_args()
 
@@ -77,8 +77,15 @@ def assertFileExists(filePath):
     assert os.path.isfile(filePath), "Trace file %s does not exist!" % filePath
     return filePath
 
-def assertValidBlockSize(blockSize):
+
+def assertValidL1BlockSize(blockSize):
     assert blockSize.isdigit(), "Block size has to be an integer!"
+    assert blockSize in validL1BlockSizes, "L1 Block size is not in allowed scope!"
+    return int(blockSize)
+
+def assertValidL2BlockSize(blockSize):
+    assert blockSize.isdigit(), "Block size has to be an integer!"
+    assert blockSize in validL2BlockSizes, "L2 Block size is not in allowed scope!"
     return int(blockSize)
 
 

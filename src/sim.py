dfedee27dc51c3c0512ad3b83897a8cc82522b73
Lior <bialik@halo-analytics.com>
2017-01-11 18:42:01 +0200

    Sim| added the printing in the end of the file

--- src/sim.py
+++ src/sim.py
@@ -6,20 +6,20 @@ from Utils import ParsingUtils  # MemoryUtils
 
 def runSimulation():
 
-    mainMemory = Memory.MainMemory()
+    mainMemory = Memory.MainMemory(config.getMainMemorySize(), )
 
     if config.getLevelsOfCache() == 1:
-        l1Cache = Memory.Cache(config.L1MemorySize, config.getBlockSizeForL1Cache(),
+        l1Cache = Memory.Cache(config.getL1MemorySize(), config.getBlockSizeForL1Cache(),
                                1, mainMemory)
     if config.getLevelsOfCache() == 2:
-        l2Cache = Memory.Cache(config.L2MemorySize, config.getBlockSizeForL2Cache(),
+        l2Cache = Memory.Cache(config.getL2MemorySize(), config.getBlockSizeForL2Cache(),
                                2, mainMemory)
-        l1Cache = Memory.Cache(config.L1MemorySize, config.getBlockSizeForL1Cache(),
+        l1Cache = Memory.Cache(config.getL1MemorySize(), config.getBlockSizeForL1Cache(),
                                1, l2Cache)
 
     mainMemory.getMemoryDataFromFile(config.options.meminFilePath)
-    # # init caches (depending on levels)
-    # totalNumberOfCycles = 0
+
+    #totalNumberOfCycles = 0
     # with open(config.getTraceFilePath()) as traceFile:
     #     for line in traceFile:
     #         if 'S' in line:
@@ -30,12 +30,15 @@ def runSimulation():
     #             numberOfCyclesBeforeCmd, srcMemoryAddressStr = ParsingUtils.parseLoadLineIntoStoreVariables(line)
     #             # numberOfCycles = MemoryUtils.loadData(srcMemoryAddressStr)  # TODO: need to  think of the right way to execute
     #
-    #         totalNumberOfCycles += numberOfCyclesBeforeCmd  # + numberOfCycles
-    #
-    # # print main memory final status into 'memout' file
-    # # print l1 cache  memory final status into file
-    # # print l2 cache  memory final status into ***2*** files
-    # # print Stats
+    #         # totalNumberOfCycles += numberOfCyclesBeforeCmd  # + numberOfCycles
+
+    # print main memory final status into 'memout' file
+    mainMemory.saveMemoryToFile(config.getMainMemoryStatusOutputFilePath())
+    # print l1 cache  memory final status into file
+    # l1Cache.saveMemoryToFile(config.getL1CacheStatusOutputFilePath())
+    # print l2 cache  memory final status into ***2*** files
+    # l2Cache.saveMemoryToFile(config.getL2CacheStatusOutputFilePath())
+    # print Stats
 
 if __name__ == "__main__":
     try:
